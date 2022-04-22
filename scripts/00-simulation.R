81b2891e8925628d80d7c1a6012ae8ef67809fc4
SangWoong Lee <sangwoong.lee@mai.utoronto.ca>
2022-04-22 16:19:09 -0400

    Simulation updated

--- scripts/00-simulation.R
+++ scripts/00-simulation.R
@@ -1,9 +1,8 @@
 ### Preamble ###
-# Purpose: Obtain and prepare data about children 12 to 59 months of age according to immunization 
-# status and method of reporting immunization status, by selected background characteristics. 
-# Author: SangWoong Lee and Young Suk
-# Email: sangwoong.lee@mail.utoronto.ca and young.suk@mail.utoronto.ca
-# Date 24 March 2022
+# Purpose: Obtain and prepare data about U.S citizens opinions on Government spending 
+# Author: SangWoong Lee 
+# Email: sangwoong.lee@mail.utoronto.ca
+# Date 22 April 2022
 # prerequisites: - 
 
 ### Worksapce set-up ###
@@ -13,134 +12,157 @@ library(tidyverse)
 
 set.seed(777)
 sample_size <- 3000
-simulated_urban_rural <- 
+simulate_national_spending <- 
   tibble(
-    variables = 
-      c(
-        rep('Urban',1),
-        rep('Rural',1)
-      ),
-    Health_record = 
-      runif(n=2,
-            min = 0,
-            max = 100),
-    Mother_record = 
-      runif(n= 2,
-            min = 0,
-            max = 100 -Health_record),
-    immunization = Mother_record + Health_record,
-    non_immunization = 100 - immunization,
-    number_of_childeren = 
-      as.numeric(rmultinom(n = 1, size = 3000, prob = rep(1/2, 2)))
+    natarms = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natchld = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natcity = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natcrime = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natdrug = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    nateduc = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natenrgy = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natenvir = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natfare = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natheal = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natmass = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natpark = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natrace = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natraod = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natsci = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    natsoc = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 3)),
+    eqwlth = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 7)),
+    health = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 4)),
+    helpsick = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 5)),
+    helppoor = 
+      round(runif(n=3000,
+                  min = 1,
+                  max = 5)),
+    year = 
+      sample(x = c("2018","2021"),
+             size = 3000,
+             replace = TRUE)
   )
-simulated_region <- 
-  tibble(
-    variables = 
-      c(
-        rep('North',1),
-        rep('Northeast',1),
-        rep('Central',1),
-        rep('South',1),
-        rep('Bangkok',1)
-      ),
-    Health_record = 
-      runif(n=5,
-            min = 0,
-            max = 100),
-    Mother_record = 
-      runif(n= 5,
-            min = 0,
-            max = 100 -Health_record),
-    immunization = Mother_record + Health_record,
-    non_immunization = 100 - immunization,
-    number_of_childeren = 
-      as.numeric(rmultinom(n = 1, size = 3000, prob = rep(1/5, 5)))
-  )
-simulated_education <- 
-  tibble(
-    variables = 
-      c(
-        rep('No_education',1),
-        rep('Primary',1),
-        rep('Secondary',1),
-        rep('Higher',1)
-      ),
-    Health_record = 
-      runif(n=4,
-            min = 0,
-            max = 100),
-    Mother_record = 
-      runif(n= 4,
-            min = 0,
-            max = 100 -Health_record),
-    immunization = Mother_record + Health_record,
-    non_immunization = 100 - immunization,
-    number_of_childeren = 
-      as.numeric(rmultinom(n = 1, size = 3000, prob = rep(1/4, 4)))
-  )
-simulated_religion <- 
-  tibble(
-    variables = 
-      c(
-        rep('Buddhist',1),
-        rep('Islam',1)
-      ),
-    Health_record = 
-      runif(n=2,
-            min = 0,
-            max = 100),
-    Mother_record = 
-      runif(n= 2,
-            min = 0,
-            max = 100 -Health_record),
-    immunization = Mother_record + Health_record,
-    non_immunization = 100 - immunization,
-    number_of_childeren = 
-      as.numeric(rmultinom(n = 1, size = 3000, prob = rep(1/2, 2)))
-  )
-simulated_total <- 
-  tibble(
-    variables = rep('total',1),
-    Health_record = (simulated_urban_rural$number_of_childeren[1] * (simulated_urban_rural$Health_record[1] *0.01)
-    + simulated_urban_rural$number_of_childeren[2] * (simulated_urban_rural$Health_record[2] *0.01)) / 3000 * 100,
-    Mother_record = (simulated_urban_rural$number_of_childeren[1] * (simulated_urban_rural$Mother_record[1] *0.01)
-                     + simulated_urban_rural$number_of_childeren[2] * (simulated_urban_rural$Mother_record[2] *0.01)) / 3000 * 100,
-    immunization = Mother_record + Health_record,
-    non_immunization = 100 - immunization,
-    number_of_childeren = 3000
-  )
-simulated_immunization_data <- rbind(simulated_urban_rural, simulated_region, simulated_education, simulated_religion,simulated_total)
   
-simulated_immunization_data$variables |>
-  unique() == c('Urban',
-                'Rural',
-                'North',
-                'Northeast',
-                'Central',
-                'South',
-                'Bangkok',
-                'No_education',
-                'Primary',
-                'Secondary',
-                'Higher',
-                'Buddhist',
-                'Islam')
-simulated_immunization_data$variables |> unique() |> length() == 14
-simulated_immunization_data$Health_record |>  min() >= 0 
-simulated_immunization_data$Health_record |>  max() <= 100
-simulated_immunization_data$Mother_record |>  min() >= 0 
-simulated_immunization_data$Mother_record |>  max() <= 100
-simulated_immunization_data$immunization |>  min() >= 0 
-simulated_immunization_data$immunization |>  max() <= 100
-simulated_immunization_data$non_immunization |>  min() >= 0 
-simulated_immunization_data$non_immunization |>  max() <= 100
-simulated_immunization_data$number_of_childeren |> min() >= 0
-simulated_immunization_data$Health_record + simulated_immunization_data$Mother_record == simulated_immunization_data$immunization
-100 - simulated_immunization_data$immunization == simulated_immunization_data$non_immunization
-simulated_immunization_data$variables |> class() =='character'
-simulated_immunization_data$Health_record |> class() =='numeric'
-simulated_immunization_data$Mother_record |> class() =='numeric'
-simulated_immunization_data$immunization |> class() =='numeric'
-simulated_immunization_data$non_immunization |> class() =='numeric'
-simulated_immunization_data$number_of_childeren |> class() =='numeric'
+simulate_national_spending$nataid |>  min() >= 0 
+simulate_national_spending$nataid |>  max() >= 3
+simulate_national_spending$natarms |>  min() >= 0 
+simulate_national_spending$natarms |>  max() >= 3
+simulate_national_spending$natchld |>  min() >= 0 
+simulate_national_spending$natchld |>  max() >= 3
+simulate_national_spending$natcity |>  min() >= 0 
+simulate_national_spending$natcity |>  max() >= 3
+simulate_national_spending$natcrime |>  min() >= 0 
+simulate_national_spending$natcrime |>  max() >= 3
+simulate_national_spending$natdrug |>  min() >= 0 
+simulate_national_spending$natdrug |>  max() >= 3
+simulate_national_spending$nateduc |>  min() >= 0 
+simulate_national_spending$nateduc |>  max() >= 3
+simulate_national_spending$natenrgy |>  min() >= 0 
+simulate_national_spending$natenrgy |>  max() >= 3
+simulate_national_spending$natenvir |>  min() >= 0 
+simulate_national_spending$natenvir |>  max() >= 3
+simulate_national_spending$natfare |>  min() >= 0 
+simulate_national_spending$natfare |>  max() >= 3
+simulate_national_spending$natheal |>  min() >= 0 
+simulate_national_spending$natheal |>  max() >= 3
+simulate_national_spending$natmass |>  min() >= 0 
+simulate_national_spending$natmass |>  max() >= 3
+simulate_national_spending$natpark |>  min() >= 0 
+simulate_national_spending$natpark |>  max() >= 3
+simulate_national_spending$natrace |>  min() >= 0 
+simulate_national_spending$natrace |>  max() >= 3
+simulate_national_spending$natraod |>  min() >= 0 
+simulate_national_spending$natraod |>  max() >= 3
+simulate_national_spending$natsci |>  min() >= 0 
+simulate_national_spending$natsci |>  max() >= 3
+simulate_national_spending$natsoc |>  min() >= 0 
+simulate_national_spending$natsoc |>  max() >= 3
+simulate_national_spending$eqwlth |>  min() >= 0 
+simulate_national_spending$eqwlth |>  max() >= 7
+simulate_national_spending$health |>  min() >= 0 
+simulate_national_spending$health |>  max() >= 5
+simulate_national_spending$helpsick |>  min() >= 0 
+simulate_national_spending$helpsick |>  max() >= 5
+simulate_national_spending$helppoor |>  min() >= 0 
+simulate_national_spending$helppoor |>  max() >= 5
+simulate_national_spending$year == 2018 | 2021
+simulate_national_spending$nataid |> class() =='numeric'
+simulate_national_spending$natarms |> class() =='numeric'
+simulate_national_spending$natchld |> class() =='numeric'
+simulate_national_spending$natcity |> class() =='numeric'
+simulate_national_spending$natcrime |> class() =='numeric'
+simulate_national_spending$natdrug |> class() =='numeric'
+simulate_national_spending$nateduc |> class() =='numeric'
+simulate_national_spending$natenrgy |> class() =='numeric'
+simulate_national_spending$natenvir |> class() =='numeric'
+simulate_national_spending$natfare |> class() =='numeric'
+simulate_national_spending$natheal |> class() =='numeric'
+simulate_national_spending$natmass |> class() =='numeric'
+simulate_national_spending$natpark |> class() =='numeric'
+simulate_national_spending$natrace |> class() =='numeric'
+simulate_national_spending$natraod |> class() =='numeric'
+simulate_national_spending$natsci |> class() =='numeric'
+simulate_national_spending$natsoc |> class() =='numeric'
+simulate_national_spending$eqwlth |> class() =='numeric'
+simulate_national_spending$health |> class() =='numeric'
+simulate_national_spending$helpsick |> class() =='numeric'
+simulate_national_spending$helppoor |> class() =='numeric'
+simulate_national_spending$year |> class() =='character'
 
